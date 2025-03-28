147df5cdb391f5f7c2d83babb362ebece859bc34
Sakshi Patil <spatil@microproindia.com>
2025-03-28 12:06:34 +0530

    eureka server service registry related changes added

--- notification-service/src/main/java/com/example/demo/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/example/demo/NotificationServiceApplication.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//Main class of  Notification service
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
