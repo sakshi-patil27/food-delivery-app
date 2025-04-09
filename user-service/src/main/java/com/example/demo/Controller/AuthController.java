c9a7e89637522aa041f68236c7126d3f64720f37
Sakshi Patil <spatil@microproindia.com>
2025-04-09 16:50:14 +0530

    hhhhhh

--- user-service/src/main/java/com/example/demo/Controller/AuthController.java
+++ user-service/src/main/java/com/example/demo/Controller/AuthController.java
@@ -1,35 +1,23 @@
 package com.example.demo.Controller;
 
-import com.example.demo.entity.AuthRequest;
-import com.example.demo.entity.RegisterRequest;
-import com.example.demo.entity.Role;
-import com.example.demo.entity.user_info;
-import com.example.demo.security.AuthService;
-import com.example.demo.security.JwtTokenProvider;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import aj.org.objectweb.asm.TypeReference;
-
-import com.example.demo.Repository.RoleRepository;
-import com.example.demo.Repository.UserRepository;
-import com.example.demo.Service.UserService;
-
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Service.UserService;
+import com.example.demo.entity.AuthRequest;
+import com.example.demo.entity.user_info;
+import com.example.demo.security.AuthService;
+import com.example.demo.security.JwtTokenProvider;
 
 @RestController
 @RequestMapping("/auth")
@@ -93,7 +81,6 @@ public class AuthController {
 	 @GetMapping("/validate")
 	    public ResponseEntity<?> validateToken() {
 	        try {
-	        	
 	        	return ResponseEntity.ok("Token is valid for user: "); 
 	        } catch (Exception e) {
 	            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
